c47f530080e3237c031f8a29946288cd0a8cf6fb
Girish <i.girish.krishna@gmail.com>
2022-11-21 16:03:43 +0530

    project 7 done.

--- 07-slider/setup/README.md
+++ 07-slider/setup/README.md
@@ -1,5 +1,12 @@
-# react-projects-7-slider
+##### lastSlide - activeSlide - nextSlide (like circle)
 
-#### IN ACTION
+###### opacity becomes 1 for only active slide and also we use overflow: hidden property.if we use use postiton: absolute, we must provide height to the element.
 
-[Gatsby-Airtable Project](https://gatsby-airtable-design-project.netlify.app/)
+##### keys to remember
+
+1. by default all the articles get the 'nextSlide' class and because of opactity: 0 in article and overflow: hidden in section, we don't see them in our UI.
+2. we display the 'index' state value. so if 'index' === 'personIndex' in map, we replace nextSlide to activeSlide.
+3. by default, for lastSlide we put the last element of the array by using 'index - 1' value. But for first, lastSlide would be the last item of the array.
+
+4. to make this work effectively, we use 'useEffect' hook.
+5. and for auto-slide we used useEffect with setInterval and clearInterval.
--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -1,9 +1,70 @@
-import React, { useState, useEffect } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
-import data from './data';
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
 function App() {
-  return <h2>slider project setup</h2>;
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  // for prev and next buttons when they reach first and last elements of the array.
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  // for auto-sliding
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 3000);
+    return clearInterval(slider); // cleanup function. important.
+  });
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>Reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, image, title, name, quote } = person;
+          // by default
+          let position = "nextSlide";
+          // if its equal to index
+          if (personIndex === index) position = "activeSlide";
+          // it should be previous of current item but for first item, prev will be the last. (like a circle)
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          )
+            position = "lastSlide";
+
+          return (
+            <article className={position} key={id}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p>{title}</p>
+              <p>{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+        <button className="prev" onClick={() => setIndex(index - 1)}>
+          <FiChevronLeft />
+        </button>
+        <button className="next" onClick={() => setIndex(index + 1)}>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
