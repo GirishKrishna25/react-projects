02857c548578dbc2da241c7ad4c44ed73a2ee9d4
Girish <i.girish.krishna@gmail.com>
2022-11-22 17:09:23 +0530

    Project 8 Done.

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,41 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
+
 function App() {
-  
-  
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // from the entire data, we are taking only required amount and putting it in 'text'
+    setText(data.slice(0, count));
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>Tired of boring lorem ipsum?</h3>
+      <h6>Note: You can generate maximum 10 paragraphs.</h6>
+      <form onSubmit={handleSubmit} className="lorem-form">
+        <label htmlFor="amount">Paragraphs:</label>
+        <input
+          type="number"
+          id="amount"
+          min="1"
+          max="10"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
