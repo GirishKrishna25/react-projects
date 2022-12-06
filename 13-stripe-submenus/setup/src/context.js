bdf3c52ca162eac98c180de4a642d113841e40fc
Girish <i.girish.krishna@gmail.com>
2022-12-06 14:02:12 +0530

    done project 13

--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,47 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, createContext, useContext } from "react";
+import sublinks from "./data";
+
+const AppContext = createContext();
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+  const [page, setPage] = useState({ page: "", links: [] });
+  const [location, setLocation] = useState({});
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openSubmenu = (text, coordinates) => {
+    const page = sublinks.find((link) => link.page === text);
+    setPage(page);
+    setLocation(coordinates);
+    setIsSubmenuOpen(true);
+  };
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isSubmenuOpen,
+        openSidebar,
+        closeSidebar,
+        openSubmenu,
+        closeSubmenu,
+        page,
+        location,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => useContext(AppContext);
+export { AppContext, AppProvider };
